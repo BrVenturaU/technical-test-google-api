e79e85bd0f7d2bb820eb036b766fe4af6ee316b6
BrVenturaU <brandonventura16@gmail.com>
2021-07-06 14:46:54 -0600

    Agrega encabezado de autorización en Swagger.

--- TechnicalTestGoogleApi/Startup.cs
+++ TechnicalTestGoogleApi/Startup.cs
@@ -33,6 +33,28 @@ namespace TechnicalTestGoogleApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechnicalTestGoogleApi", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[]{}
+                    }
+                });
             });
         }
 
