8dcc3230b2e400447d6d63dc629208af69eb49e7
BrVenturaU <brandonventura16@gmail.com>
2021-07-06 21:52:40 -0600

    Agrega middleware para respuesta sin autorizacion al usuario.

---
+++ TechnicalTestGoogleApi/Middlewares/ClientUnauthorizeMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using TechnicalTestGoogleApi.Utils;
+
+namespace TechnicalTestGoogleApi.Middlewares
+{
+    public class ClientUnauthorizeMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ClientUnauthorizeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next(context);
+            var statusCode = context.Response.StatusCode;
+            string contentType = "application/json";
+            if (statusCode == (int)HttpStatusCode.Unauthorized)
+            {
+                context.Response.ContentType = contentType;
+                await context.Response.WriteAsync(new ApiResponse(statusCode)
+                {
+                    Message = "La sesión actual no es válida."
+                }.ToString()) ;
+            }
+            else if(statusCode == (int)HttpStatusCode.Forbidden)
+            {
+                context.Response.ContentType = contentType;
+                await context.Response.WriteAsync(new ApiResponse(statusCode)
+                {
+                    Message = "Acceso denegado."
+                }.ToString());
+            }
+        }
+    }
+
+    public static class ClientUnauthorizeMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseClientUnauthorizeMiddleware(this IApplicationBuilder app) {
+            return app.UseMiddleware<ClientUnauthorizeMiddleware>();
+        }
+    }
+}
--- TechnicalTestGoogleApi/Startup.cs
+++ TechnicalTestGoogleApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TechnicalTestGoogleApi.Extensions;
+using TechnicalTestGoogleApi.Middlewares;
 using TechnicalTestGoogleApi.Utils;
 
 [assembly: ApiConventionType(typeof(DefaultApiConventions))]
@@ -33,6 +34,7 @@ namespace TechnicalTestGoogleApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseClientUnauthorizeMiddleware();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
